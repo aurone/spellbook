07a26cf062b5780b815e85d1223511ebe7603c1d
Andrew Dornbush <andrew.dornbush@gmail.com>
2015-10-06 19:03:16 -0400

    minor commenting in locateeigen module

--- cmake/Modules/LocateEigen.cmake
+++ cmake/Modules/LocateEigen.cmake
@@ -1,17 +1,17 @@
 macro(locate_eigen)
     # Attempt to find Eigen3 through a variety of methods:
     
-    # officially supported cmake find module
+    # check for officially supported cmake find module
     find_package(Eigen3 QUIET)
     if (${EIGEN3_FOUND})
-        set(EIGEN_INCLUDES ${EIGEN3_INCLUDE_DIR})
+        set(Eigen_INCLUDE_DIRS ${EIGEN3_INCLUDE_DIR})
     else()
-        # unofficially suported cmake find module
+        # check for unofficially suported cmake find module
         find_package(Eigen QUIET)
         if (${EIGEN_FOUND})
-            set(EIGEN_INCLUDES ${Eigen_INCLUDE_DIRS})
+#            set(EIGEN_INCLUDES ${Eigen_INCLUDE_DIRS})
         else()
-            # pkg-config file
+            # check for officially supported pkg-config file
             find_package(PkgConfig REQUIRED)
             pkg_check_module(Eigen REQUIRED Eigen3)
         endif()
