2e02a55404225fd02cd23d90cf9aee59e7b3830f
Andrew Dornbush <andrew.dornbush@gmail.com>
2016-04-25 20:52:40 -0400

    add derived pi constants and natural log constant

--- spellbook/src/include/spellbook/math/constants.h
+++ spellbook/src/include/spellbook/math/constants.h
@@ -14,7 +14,14 @@ class constants<double>
 {
 public:
 
-    static double pi() { return 3.141592653589793238462643383275028841; }
+    static constexpr double pi() { return 3.141592653589793238462643383275028841; }
+    static constexpr double sixth_pi() { return pi() / 6.0; }
+    static constexpr double half_pi() { return pi() / 2.0; }
+    static constexpr double quarter_pi() { return pi() / 4.0; }
+    static constexpr double two_pi() { return 2.0 * pi(); }
+    static constexpr double sqrt_two_pi() { return sqrt(two_pi()); }
+
+    static constexpr double e() { 2.71828182845904523536028747135266249775724709369995; }
 };
 
 template <>
@@ -22,7 +29,14 @@ class constants<float>
 {
 public:
 
-    static float pi() { return 3.141592653589793238462643383275028841F; }
+    static constexpr float pi() { return 3.141592653589793238462643383275028841F; }
+    static constexpr float sixth_pi() { return pi() / 6.0f; }
+    static constexpr float half_pi() { return pi() / 2.0f; }
+    static constexpr float quarter_pi() { return pi() / 4.0f; }
+    static constexpr float two_pi() { return 2.0f * pi(); }
+    static constexpr float sqrt_two_pi() { return sqrt(two_pi()); }
+
+    static constexpr float e() { 2.71828182845904523536028747135266249775724709369995F; }
 };
 
 template <>
@@ -30,7 +44,14 @@ class constants<long double>
 {
 public:
 
-    static long double pi() { return 3.141592653589793238462643383275028841l; }
+    static constexpr long double pi() { return 3.141592653589793238462643383275028841L; }
+    static constexpr long double sixth_pi() { return pi() / 6.0L; }
+    static constexpr long double half_pi() { return pi() / 2.0L; }
+    static constexpr long double quarter_pi() { return pi() / 4.0L; }
+    static constexpr long double two_pi() { return 2.0L * pi(); }
+    static constexpr long double sqrt_two_pi() { return sqrt(two_pi()); }
+
+    static constexpr long double e() { 2.71828182845904523536028747135266249775724709369995L; }
 };
 
 } // namespace au
